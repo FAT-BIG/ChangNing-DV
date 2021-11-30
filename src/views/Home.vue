b922c5ab7002d6dcf7273cee089487871c3438b5
honghaiquan <hhq@moreyou.cn>
2021-11-30 18:26:33 +0800

    add scrollList

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,85 @@
 <template>
-<div style="height: 40vh">
-  <BaseBar />
-</div>
+<!--  <div style="height: 40vh">-->
+<!--    <base-bar/>-->
+<!--  </div>-->
+  <scroll-list :list="listData" class="ranking-list">
+    <div>
+      <template v-for="(item,index) in listData">
+        <div class="ranking-list-item" v-if="index < 10">
+          <div class="item-left">
+            <div class="sn" :class="{'first-class':index < 3}">{{ index + 1 }}</div>
+            <div class="title">{{ item.name }}</div>
+          </div>
+          <div class="value">{{ item.total }}</div>
+        </div>
+      </template>
+    </div>
+  </scroll-list>
+
 </template>
 
 <script>
-import {ref,defineComponent} from 'vue'
+import {ref, defineComponent} from 'vue'
 import BaseBar from '../components/charts/BaseBar.vue'
+import ScrollList from '../components/kits/ScrollList/index.vue'
+
 export default defineComponent({
   name: "Home",
-  components:{BaseBar}
+  components: {
+    BaseBar,
+    ScrollList
+  },
+  setup() {
+    const listData = ref([
+      {
+        name: '工专路一号店',
+        total: 323234
+      },
+      {
+        name: '工专路二号店',
+        total: 320300
+      },
+      {
+        name: '工专路一号店',
+        total: 323234
+      },
+      {
+        name: '工专路二号店',
+        total: 320300
+      },
+      {
+        name: '工专路一号店',
+        total: 323234
+      },
+      {
+        name: '工专路二号店',
+        total: 320300
+      },
+      {
+        name: '工专路一号店',
+        total: 323234
+      },
+      {
+        name: '工专路二号店',
+        total: 320300
+      },
+      {
+        name: '工专路二号店',
+        total: 320300
+      },
+      {
+        name: '工专路一号店',
+        total: 323234
+      },
+      {
+        name: '工专路二号店',
+        total: 320300
+      }
+    ])
+    return {
+      listData
+    }
+  }
 })
 </script>
 
