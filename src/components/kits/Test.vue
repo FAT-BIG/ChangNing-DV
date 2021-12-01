96343b8e6376efb48120ef2d3f378298c271dec2
honghaiquan <hhq@moreyou.cn>
2021-12-01 09:09:59 +0800

    add element-plus & component auto register

---
+++ src/components/kits/Test.vue
@@ -0,0 +1,21 @@
+<template>
+  <el-button>button</el-button>
+</template>
+
+<script lang="ts">
+import {ref, defineComponent} from 'vue'
+
+export default defineComponent({
+  name: "Test",
+  setup() {
+    const value = ref(0)
+    return {
+      value
+    }
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,33 +1,34 @@
 <template>
-<!--  <div style="height: 40vh">-->
-<!--    <base-bar/>-->
-<!--  </div>-->
-  <scroll-list :list="listData" class="ranking-list">
-    <div>
-      <template v-for="(item,index) in listData">
-        <div class="ranking-list-item" v-if="index < 10">
-          <div class="item-left">
-            <div class="sn" :class="{'first-class':index < 3}">{{ index + 1 }}</div>
-            <div class="title">{{ item.name }}</div>
-          </div>
-          <div class="value">{{ item.total }}</div>
-        </div>
-      </template>
-    </div>
-  </scroll-list>
+  <test />
+  <div style="height: 40vh">
+    <base-bar/>
+  </div>
+<!--  <scroll-list :list="listData" class="ranking-list">-->
+<!--    <div>-->
+<!--      <template v-for="(item,index) in listData">-->
+<!--        <div class="ranking-list-item" v-if="index < 10">-->
+<!--          <div class="item-left">-->
+<!--            <div class="sn" :class="{'first-class':index < 3}">{{ index + 1 }}</div>-->
+<!--            <div class="title">{{ item.name }}</div>-->
+<!--          </div>-->
+<!--          <div class="value">{{ item.total }}</div>-->
+<!--        </div>-->
+<!--      </template>-->
+<!--    </div>-->
+<!--  </scroll-list>-->
 
 </template>
 
 <script>
 import {ref, defineComponent} from 'vue'
 import BaseBar from '../components/charts/BaseBar.vue'
-import ScrollList from '../components/kits/ScrollList/index.vue'
+// import ScrollList from '../components/kits/ScrollList.vue'
 
 export default defineComponent({
   name: "Home",
   components: {
     BaseBar,
-    ScrollList
+    // ScrollList
   },
   setup() {
     const listData = ref([
