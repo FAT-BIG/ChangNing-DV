afd40bce2542cf079458cac67f8842a84d13125c
honghaiquan <hhq@moreyou.cn>
2021-12-02 17:34:39 +0800

    card

--- src/components/kits/Card.vue
+++ src/components/kits/Card.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="card">
-    <div class="card-title" v-if="title">
+    <div class="card-title" :data-title="title" v-if="title">
       {{ title }}
     </div>
     <div class="card-content">
@@ -32,30 +32,40 @@ export default defineComponent({
   box-shadow: inset 0px 0px 88px 8px rgba(16, 108, 222, 0.2), inset 0px 0px 12px 2px rgba(0, 114, 255, .8);
   width: 400px;
   min-height: 240px;
+  border-radius: 4px;
+  overflow: hidden;
 
   .card-title {
     position: absolute;
     left: 0;
     top: 0;
-    height: 40px;
-    display: flex;
-    align-items: center;
-    padding: 0 12px;
+    height: 42px;
+    padding: 8px 24px 0 24px;
     color: $text-color;
     font-size: 14px;
     z-index: 2;
-    background: rgb(22, 48, 128);
+
+    &::after {
+      position: absolute;
+      left: 0px;
+      top: 0;
+      padding: 8px 24px 0 24px;
+      content: attr(data-title);
+      width: 100%;
+      height: 100%;
+    }
 
     &::before {
       position: absolute;
-      left: 8px;
+      left: 0px;
       top: 0;
       content: '';
       width: 100%;
       height: 100%;
-      box-shadow: inset 0px 0px 88px 8px rgba(16, 108, 222, 0.2), inset 0px 0px 12px 2px rgba(0, 114, 255, .8);
-      transform: perspective(.8em) rotateX(-5deg);
-
+      box-shadow: inset 0px 0px 12px 2px rgba(0, 114, 255, .8);
+      transform-origin: top;
+      background: rgb(22, 48, 128);
+      transform: perspective(.6em) rotateX(-4deg);
     }
   }
 }
